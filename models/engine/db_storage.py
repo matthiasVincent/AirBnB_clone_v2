942c23e6e047595cf1cbf28d523868b0cb76bc95
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-11-18 12:19:06 -0800

    do some tweaking

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 """Defines the DBStorage engine."""
 from os import getenv
-# from models.base_model import Base
-# from models.base_model import BaseModel
-# from models.amenity import Amenity
-# from models.city import City
-# from models.place import Place
-# from models.review import Review
-# from models.state import State
-# from models.user import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import scoped_session
@@ -101,7 +93,3 @@ class DBStorage:
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
-
-    def close(self):
-        """Close the working SQLAlchemy session."""
-        self.__session.close()
