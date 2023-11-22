f0119eb7460b073e35584560499b89d2ce98cf7e
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-11-22 15:12:44 -0800

    Revert "another change to models"
    This reverts commit fa1d19e0741ba9563640a45dcfe8c8d3846a2d81.

--- models/user.py
+++ models/user.py
@@ -18,12 +18,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship(
-                "Place", backref="user",
-                cascade="all, delete, delete-orphan")
-        reviews = relationship(
-                "Review", backref="user",
-                cascade="all, delete, delete-orphan")
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
@@ -33,3 +29,9 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
