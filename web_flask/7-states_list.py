0f05a83a663165faf7e6ad80a6752b3dcaa84fcd
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-12-24 16:49:23 -0800

    edit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
 """
-from models import storage
+Script start a flask web application with
+requirements:
+    Listen on 0.0.0.0 port 5000,
+    Use storage to fetch data from engines(FileStorage or DBStorage)
+    Remove current session after each request using @app.teardown_appcontext
+    on a given method
+    Routes:
+        /states_list: display a HTML page with state information
+    required to use the option, strict_slashes=False
+"""
+
 from flask import Flask
+from models import storage
 from flask import render_template
 
-app = Flask(__name__)
-
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+app = Flask(__file__, template_folder="web_flask/templates")
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """remove the current sqlalchemy session"""
     storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """display state_list alphabetically in a HTML page"""
+    all_state_dict = storage.all("State")
+    all_state = [values for values in all_state_dict.values()]
+
+    # print(all_state)
+    return render_template("7-state_list.html", all_state=all_state)
 
 
 if __name__ == "__main__":
