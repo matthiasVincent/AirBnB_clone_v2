a6758614e27e1aaa8544ce15e440dfeecdfd387c
Matthias Sunday Oduh <111181664+matthiasVincent@users.noreply.github.com>
2023-12-27 07:49:50 +0100

    use remove on Session object

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -69,8 +69,8 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         """close session after response"""
-        self.__session.close()
+        self.__session.remove()
