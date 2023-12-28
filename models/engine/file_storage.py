f6fbbf39977ab3ce83f22a7862487c57b5a84ae0
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-12-28 06:02:30 -0800

    Back to my earlier code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ Contains the FileStorage class
 """
 
 import json
+import models
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -55,7 +56,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
@@ -66,5 +67,5 @@ class FileStorage:
                 del self.__objects[key]
 
     def close(self):
-        """call reload() method for deserializing the JSON file to objects"""
+        """reload data after response"""
         self.reload()
--- models/state.py
+++ models/state.py
@@ -14,7 +14,9 @@ class State(BaseModel, Base):
     if models.storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         name = ""
 
@@ -27,7 +29,7 @@ class State(BaseModel, Base):
         def cities(self):
             """getter for list of city instances related to the state"""
             city_list = []
-            all_cities = models.storage.all(City)
+            all_cities = models.storage.all("City")
             for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
