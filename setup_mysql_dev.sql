e2ad6427e93c0d9174dd77bae413e8e1ed2a05a1
Matthias Sunday Oduh <sundayoduhmatthias@gmail.com>
2023-12-28 15:43:44 +0000

    prev files from web-01

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,2 +1,17 @@
-insert into states values( "Alabama", "421a55f1-7d82-45d9-b54c-a76916479545", "2017-03-25 19:42:40",  "2017-03-25 19:42:40");
-select * from states;
+-- Creates a MySQL server with:
+--   Database hbnb_dev_db.
+--   User hbnb_dev with password hbnb_dev_pwd in localhost.
+--   Grants all privileges for hbnb_dev on hbnb_dev_db.
+--   Grants SELECT privilege for hbnb_dev on performance_schema.
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER
+    IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES
+   ON `hbnb_dev_db`.*
+   TO 'hbnb_dev'@'localhost';
+GRANT SELECT
+   ON `performance_schema`.*
+   TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
